17a6728c7da75004886704a071bc3986125d7b62
arkzuse <88483186+arkzuse@users.noreply.github.com>
2024-09-18 22:20:52 +0530

    add: input file read

--- utils/fileHandler.go
+++ utils/fileHandler.go
@@ -3,6 +3,8 @@ package utils
 import (
 	"fmt"
 	"os"
+	"strconv"
+	"strings"
 )
 
 func WriteInputFile(fileName string, contest string, task string, input []string) error {
@@ -20,6 +22,57 @@ func WriteInputFile(fileName string, contest string, task string, input []string
 	}
 
 	return nil
+}
+
+func ReadInputFile(fileName string) ([]string, error) {
+	file, err := os.OpenFile(fileName, os.O_RDONLY, 0644)
+	if err != nil {
+		return nil, err
+	}
+
+	var input []string
+
+	defer file.Close()
+
+	// read whole file
+	stat, err := file.Stat()
+	if err != nil {
+		return nil, err
+	}
+
+	bs := make([]byte, stat.Size())
+	_, err = file.Read(bs)
+	if err != nil {
+		return nil, err
+	}
+
+	// convert bytes to string
+	str := string(bs)
+
+	var elem string
+	lines := strings.Split(str, "\n")
+	inputSize, err := strconv.Atoi(strings.TrimSpace(lines[2]))
+	var count = 0
+	if err != nil {
+		return nil, fmt.Errorf("failed to parse input sample size")
+	}
+	for _, l := range lines {
+		if count == inputSize+2 {
+			break
+		}
+		if l == "" {
+			count++
+			input = append(input, elem)
+			elem = ""
+		} else {
+			elem += l + "\n"
+		}
+	}
+	if elem != "" {
+		input = append(input, elem)
+	}
+
+	return input, nil
 }
 
 func inputFileContent(contest string, task string, input []string) string {
